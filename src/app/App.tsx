8e371a0ef04c26612f52d709864a8a0c693a59a1
lmayegun <lmayegun@yahoo.com>
2020-08-02 15:44:55 +0100

    cleanup app file system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,32 +3,15 @@ import logo from './logo.svg';
 
 import '../styles/App.css';
 import '../styles/App.scss';
+
+import ReactRouterConfig from './route-systems/ReactRouterConfig';
 import Wrapper from './Wrapper';
 
 import { Button } from 'react-bootstrap';
 
 const App: React.FC = ()=> {
   return (
-    <div className="App">
-      <Wrapper>
-        How long have you been smiling?
-        <Button variant="primary">Primary</Button>
-      </Wrapper>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactRouterConfig />
   );
 }
 
--- src/app/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.h1`
-  color: palevioletred;
-  font-weight: bold;
-`;
-
-export default Wrapper;
