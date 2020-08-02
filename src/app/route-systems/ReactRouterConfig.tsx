77d1907b5baa9c160a619c50ae5c440c1f978e97
lmayegun <lmayegun@yahoo.com>
2020-08-02 15:44:23 +0100

    add ReactRouterConfig component

---
+++ src/app/route-systems/ReactRouterConfig.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainLayout from '../app-layout/MainLayout';
+
+const ReactRouterConfig: React.FC = ()=>{
+    return(
+        <MainLayout />
+    );
+};
+
+export default ReactRouterConfig;
