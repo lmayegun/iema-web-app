87c9f3b30fae22fc436852f881061f2d4ebdd6dd
lmayegun <lmayegun@yahoo.com>
2020-08-03 18:15:09 +0100

    include transform logo

---
+++ src/app/app-layout/shared-components/TransformLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransformLogo: React.FC = ()=>{
+    return(
+        <p>
+            <img src={`https://transform.iema.net/sites/default/themes/custom/transform/logo.png`} alt={'transform logo'} />
+        </p>
+    );
+};
+
+export default TransformLogo;
