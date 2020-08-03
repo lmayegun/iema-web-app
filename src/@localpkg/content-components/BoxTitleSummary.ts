21cebd66a8594742d453af4d6767d5101010b169
lmayegun <lmayegun@yahoo.com>
2020-08-03 22:35:37 +0100

    add Interface to JumbotronTeaser component and add BoxTitleSummary

---
+++ src/@localpkg/content-components/BoxTitleSummary.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const BoxTitleSummary = styled.div`
+    h3{
+        font: 24px 'MuseoSlab-700',Helvetica,Arial,sans-serif;
+    }
+    p {
+        font-size: 12px;
+    }
+`;
+
+export default BoxTitleSummary;
--- src/@localpkg/content-components/JumbotronTeaser.tsx
+++ src/@localpkg/content-components/JumbotronTeaser.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
-import {Jumbotron, Row, Col} from 'react-bootstrap'; 
+import {Jumbotron, Row, Col} from 'react-bootstrap';
+import styled from 'styled-components';
 
-const JumbotronTeaser: React.FC = ()=>{
+import BoxTitleSummary from './BoxTitleSummary';
+
+interface JumbotronTeaserProps {
+    className?: string;
+}
+
+const JumbotronTeaser: React.FC<JumbotronTeaserProps> = (props)=>{
     return(
-        <Jumbotron>
-            <Row>
-                <Col md={7}>
-                    <img 
-                        src={`https://transform.iema.net/sites/default/files/styles/new_hero_image_720_x_460/public/s3/hero/water-vole-by-philip-braude.jpg?itok=ADwDejDH`} 
-                        alt={``}
-                        className={`img-responsive`}
-                    />
-                </Col>
-                <Col md={5}>
-                    gsgg
-                </Col>
-            </Row>
-        </Jumbotron>
+        <div className={props.className}>
+            <Jumbotron>
+                <Row>
+                    <Col md={7}>
+                        <img 
+                            src={`https://transform.iema.net/sites/default/files/styles/new_hero_image_720_x_460/public/s3/hero/water-vole-by-philip-braude.jpg?itok=ADwDejDH`} 
+                            alt={``}
+                            className={`img-responsive`}
+                        />
+                    </Col>
+                    <Col md={5}>
+                        <div className={`jumbotron-content text-center`}>
+                            <BoxTitleSummary>
+                                <h3> Quarter of UK's native mammals now at risk of extinction </h3>
+                                <p> Almost a quarter of Britain's native mammals are now at “imminent risk” of extinction, according to the Mammal Society's first official endangered list. </p>
+                            </BoxTitleSummary>
+                        </div>
+                    </Col>
+                </Row>
+            </Jumbotron>
+        </div>
     );
 };
 
-export default React.memo(JumbotronTeaser); 
+const JumbotronTeaserStyled = styled(JumbotronTeaser)`
+    .jumbotron-content{
+        margin-top: 120px;
+        margin-left: -120px;
+        max-width: 100%;
+        min-height: 220px;
+        background: white;
+        padding: 36px 20px 20px;
+    }
+`;
+
+export default React.memo(JumbotronTeaserStyled); 
