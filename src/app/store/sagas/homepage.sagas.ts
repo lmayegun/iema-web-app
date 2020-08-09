8ab05f94245e7f7dcc521cef7c44bb0802c2aeb7
lmayegun <lmayegun@yahoo.com>
2020-08-09 10:49:25 +0100

    request jumbotron article from node rest endpoint

--- src/app/store/sagas/homepage.sagas.ts
+++ src/app/store/sagas/homepage.sagas.ts
@@ -1,14 +1,26 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 import {articleActionsId, homepageReducersId} from 'src/app/main/types';
 
 function* getHomepageArticlesWorker(payload : any) {
   switch (payload.payload.reducer){
     case homepageReducersId.NEWS_JUMBOTRON:{
-      yield put({
-              type:homepageReducersId.NEWS_JUMBOTRON_SUCCESS, 
-              payload: 'a news for jumbotron'
-            });
+      try{
+        const request = yield axios.get(`https://d8-recruiter-rest-simulator.herokuapp.com/api/articles/?category=news`)
+                              .then( res => {
+                                return res.data;
+                              })
+                              .catch(err =>{
+                                console.log(err);
+                              });
+        yield put({
+          type:homepageReducersId.NEWS_JUMBOTRON_SUCCESS, 
+          payload: request.slice(0,1)
+        });
+      }catch(e){
+
+      }
       break;
     }
     case homepageReducersId.NEWS_TEASER: {
@@ -22,7 +34,7 @@ function* getHomepageArticlesWorker(payload : any) {
 }
 
 export function* watchGetHomepageArticles() {
-  yield takeLatest(
+  yield takeEvery(
     articleActionsId.GET_HOMEPAGE_ARTICLES,
     getHomepageArticlesWorker
   );
