668fc7c9daf5cd479127ffe602391fdaa9434832
lmayegun <lmayegun@yahoo.com>
2020-08-14 20:15:20 +0100

    add SocialBox component

--- src/app/app-layout/Footer/FooterTopRow.tsx
+++ src/app/app-layout/Footer/FooterTopRow.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import {Col} from 'react-bootstrap';
 
-import {IemaLogo, TransformLogo, SearchInput} from 'src/app/app-layout/shared-components';
+import {IemaLogo, TransformLogo, SocialBox } from 'src/app/app-layout/shared-components';
 
 const FooterTopRow: React.FC = ()=>{
   return(
     <FooterTopRowStyled>
       <div className={'row footer-top-row'}>
         <Col md={4}> <TransformLogo/> </Col>
-        <Col md={4}> Transform </Col>
+        <Col md={4}> <SocialBox/> </Col>
         <Col md={4}> <IemaLogo /> </Col>
       </div>
     </FooterTopRowStyled>
--- src/app/app-layout/shared-components/index.ts
+++ src/app/app-layout/shared-components/index.ts
@@ -1,3 +1,4 @@
 export {default as IemaLogo} from './IemaLogo';
 export {default as TransformLogo} from './TransformLogo';
 export {default as SearchInput} from './SearchInput';
+export {default as SocialBox} from './SocialBox';
