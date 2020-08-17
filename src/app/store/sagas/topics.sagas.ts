305dd1fca7042ffa7a48b78d7b37b4deafa028e2
lmayegun <lmayegun@yahoo.com>
2020-08-17 16:18:07 +0100

    setup Topic sagas

---
+++ src/app/store/sagas/topics.sagas.ts
@@ -0,0 +1,31 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+import {articleActionsId} from 'src/app/main/types';
+
+function* getTopicsTopRegionWorker(payload : any) {
+  const {topic} = payload.payload;
+
+  try{
+    const request = yield axios.get(`https://d8-recruiter-rest-simulator.herokuapp.com/api/articles/?category=${topic}&limit=3`)
+                          .then( res => {
+                            return res.data;
+                          })
+                          .catch(err =>{
+                            console.log(err);
+                          });
+    yield put({
+      type:'[TOPICS TOP REGION] SUCCESS', 
+      payload: request
+    });
+  }catch(e){
+
+  }
+}
+
+export function* watchGetTopicArticles() {
+  yield takeEvery(
+    articleActionsId.GET_TOPICS_TOP_REGION,
+    getTopicsTopRegionWorker
+  );
+}
